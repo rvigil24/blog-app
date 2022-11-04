4171d996326afd20364c98e61b3eca9cf0035e7a
Ruben Vigil <rvigil@applaudostudios.com>
2022-11-04 14:09:18 -0600

    Delete client folder

--- client/src/BlogApp.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const BlogApp = () => {
-  return <div>BlogApp</div>;
-};
--- client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BlogApp } from './BlogApp';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BlogApp />
-  </React.StrictMode>
-);
